e47207d191d0f10e475ed64957a9003f4c515349
Bard1en <greger.grym@gmail.com>
2024-02-14 08:23:52 +0100

    formulär1

--- scirpt.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var tackElement = document.getElementById("tackElement");
-
-    // Lägg till en timeout för att ta bort CSS-klassen efter 3 sekunder (kan justeras)
-    setTimeout(function () {
-        tackElement.classList.remove("animate");
-    }, 3000);
-});
